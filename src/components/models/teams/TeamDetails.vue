42450b89b2e37a5f83aeaac9058b94a0cf61b18d
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2017-02-24 23:27:37 +0000

    scaffolded first TeamDetail component

---
+++ src/components/models/teams/TeamDetails.vue
@@ -0,0 +1,30 @@
+<style></style>
+<template>
+    <div>
+        {{team}}
+    </div>
+</template>
+<script>
+    import TeamsService from '../../../services/ads/TeamsService'
+
+    const service = new TeamsService();
+    export default {
+        name: 'team-details',
+        data() {
+            return {
+                team: {}
+            };
+        },
+        mounted() {
+            service.getOne(this.$route.params.teamId).then(
+                (data) => {
+                    this.team = data.body.payload;
+                },
+                (error) => {
+                    console.log(error)
+                }
+            );
+        }
+    }
+
+</script>
