d1a9c145011fc84575494606c460e20e2a989011
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2017-02-24 22:58:18 +0000

    added first Team service scaffolding

---
+++ src/services/ads/TeamsService.js
@@ -0,0 +1,12 @@
+import AdsService from './AdsService';
+
+const ENDPOINT = 'teams';
+
+export default class TeamsService extends AdsService {
+    getOne(id) {
+        return this.get(`${ENDPOINT}/${id}`);
+    }
+    getAll() {
+        return this.get(`${ENDPOINT}/${id}`);
+    }
+};
