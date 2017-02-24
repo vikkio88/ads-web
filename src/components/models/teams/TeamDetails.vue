22c8d3baf3ebfd5bf2d4f88f29d26fb3062aa77e
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2017-02-24 23:41:03 +0000

    fixed bug on sortable fields

--- src/components/models/teams/TeamDetails.vue
+++ src/components/models/teams/TeamDetails.vue
@@ -3,6 +3,7 @@
     <div>
         <div class="jumbotron">
             <h1>{{team.name}}</h1>
+            <h2>{{team.nationality}}</h2>
         </div>
         <b-form-fieldset horizontal label="Filter" class="col-6" :label-size="2">
             <b-form-input v-model="filter" placeholder="Type to Search"></b-form-input>
@@ -21,13 +22,13 @@
             return {
                 team: {},
                 fields: {
-                    name: { label: 'Name', sortable: true },
                     surname: { label: 'Surname', sortable: true },
-                    nationality: { label: 'Nationality', sortable: true },
+                    name: { label: 'Name', sortable: false },
+                    nationality: { label: 'Nationality', sortable: false },
                     age: { label: 'Age', sortable: true },
                     role: { label: 'Role', sortable: false },
                     skillAvg: { label: 'Skills', sortable: true },
-                    val: { label: 'Value', sortable: true },
+                    val: { label: 'Value', sortable: false },
                     actions: { label: 'Actions' }
                 },
                 filter: null
