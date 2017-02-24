696963741d8799abcba30cee8e1ad04db1432dd5
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2017-02-24 23:27:59 +0000

    add new view components

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,25 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-
 Vue.use(Router)
+
+import Teams from '../components/models/teams/Teams'
+import TeamDetails from '../components/models/teams/TeamDetails'
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'Hello'
+      path: '/teams',
+      name: 'teams',
+      component: Teams
+    },
+    {
+      path: '/teams/:teamId',
+      name: 'team',
+      component: TeamDetails
+    },
+    {
+      path: '*',
+      redirect: '/teams'
     }
   ]
 })
