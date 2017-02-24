24965979068ecdb98e4b065a0786806abfa17ff3
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2017-02-24 23:27:48 +0000

    Scaffolded Teams Table

---
+++ src/components/models/teams/Teams.vue
@@ -0,0 +1,48 @@
+<style></style>
+<template>
+    <div>
+         <b-form-fieldset horizontal label="Filter" class="col-6" :label-size="2">
+                    <b-form-input v-model="filter" placeholder="Type to Search"></b-form-input>
+                </b-form-fieldset>
+        <b-table stripped hover :items="teams" :fields="fields" :filter="filter">
+            <template slot="actions" scope="item">
+                <b-btn size="sm" @click="details(item.item.id)">Roster</b-btn>
+            </template>
+        </b-table>
+    </div>
+</template>
+<script>
+    import TeamsService from '../../../services/ads/TeamsService'
+
+    const service = new TeamsService();
+    export default {
+        name: 'team-details',
+        data() {
+            return {
+                teams: [],
+                fields: {
+                    name: { label: 'Name', sortable: true },
+                    nationality: { label: 'Nationality', sortable: true },
+                    actions: { label: 'Actions' }
+                },
+                filter: null
+            };
+        },
+        mounted() {
+            service.getAll().then(
+                (data) => {
+                    this.teams = data.body.payload;
+                },
+                (error) => {
+                    console.log(error)
+                }
+            );
+        },
+        methods: {
+            details(id) {
+                this.$router.push({ name: 'team', params: { teamId : id } })
+            }
+        }
+    }
+
+</script>
