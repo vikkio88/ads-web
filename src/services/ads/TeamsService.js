e014e30a3f2f684a420a6b4f0e7c94f4187b53bc
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2017-02-25 22:41:49 +0000

    adding new methods to new endpoints

--- src/services/ads/TeamsService.js
+++ src/services/ads/TeamsService.js
@@ -3,8 +3,11 @@ import AdsService from './AdsService';
 const ENDPOINT = 'teams';
 
 export default class TeamsService extends AdsService {
-    getOne(id) {
-        return this.get(`${ENDPOINT}/${id}`);
+    getOneWithRoster(id) {
+        return this.get(`${ENDPOINT}/${id}/roster`);
+    }
+    getOneWithMatches(id) {
+        return this.get(`${ENDPOINT}/${id}/matches`);
     }
     getAll() {
         return this.get(`${ENDPOINT}`);
