df11467cbc5a43403d8ebf80ed5b2fef0d408130
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2017-02-25 22:40:10 +0000

    moved Team Table to standalone component

---
+++ src/components/shared/matches/TeamMatchTable.vue
@@ -0,0 +1,39 @@
+<style></style>
+<template>
+    <div>
+        <table v-if="matches.length" class="table table-striped table-hover">
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Day</th>
+                    <th>Opponent</th>
+                    <th>Result</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="match in matches">
+                    <td></td>
+                    <td>{{match.date}}</td>
+                    <td>{{match[opponent].name}} ({{match[opponent].nationality}})</td>
+                    <td v-if="match.simulated"> {{match.goal_home}} - {{match.goal_away}}</td>
+                    <td v-else=""> ? - ?</td>
+                </tr>
+            </tbody>
+        </table>
+        <div class="card" v-else>
+            <div class="card-block">
+                No Matches Yet
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+    export default {
+        name: "singleTeam",
+        props: {
+            opponent: { type: String, default: 'away_team' },
+            matches: { type: Array, default: () => [] }
+        }
+    }
+
+</script>
