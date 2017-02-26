1278a0d9591478974c31a5a78e0749d1ecbbb9f5
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2017-02-26 18:09:19 +0000

    creating team table component

---
+++ src/components/shared/teams/TeamsTable.vue
@@ -0,0 +1,41 @@
+<style></style>
+<template>
+    <div>
+        <table v-if="teams.length" class="table table-striped table-hover">
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Name</th>
+                    <th>Country</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="team in teams">
+                    <td></td>
+                    <td>
+                        <router-link :to="{ name: 'team', params: { teamId: team.id } }">
+                            {{team.name}}
+                        </router-link>
+                    </td>
+                    <td>
+                        <flag :iso="team.nationality" />
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+        <div class="card" v-else>
+            <div class="card-block">
+                No Teams ...
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+    export default {
+        name: "teamsTable",
+        props: {
+            teams: { type: Array, default: () => [] }
+        }
+    }
+
+</script>
