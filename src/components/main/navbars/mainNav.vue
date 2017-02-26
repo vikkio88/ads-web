e51ca4e24eff9523fb18ee1898c969dbed1078ba
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2017-02-26 01:00:55 +0000

    fixing links on main nav

--- src/components/main/navbars/mainNav.vue
+++ src/components/main/navbars/mainNav.vue
@@ -1,19 +1,26 @@
 <style>
-    .navbar-brand span{
+    .navbar-brand span {
         color: white;
     }
 </style>
 <template>
     <b-navbar toggleable full type="inverse" variant="inverse">
         <b-nav-toggle target="nav_collapse" />
-        <a class="navbar-brand" to="/">
+        <router-link class="navbar-brand" to="/">
             <span>AD Simulator</span>
-        </a>
+        </router-link>
         <b-collapse isNav id="nav_collapse">
             <b-nav isNavBar>
-                <b-nav-item>Support</b-nav-item>
-                <b-nav-item>Docs</b-nav-item>
-                <b-nav-item>Contact Us</b-nav-item>
+                <b-nav-item>
+                    <router-link to="/leagues">
+                        Leagues
+                    </router-link>
+                </b-nav-item>
+                <b-nav-item>
+                    <router-link to="/teams">
+                        Teams
+                    </router-link>
+                </b-nav-item>
             </b-nav>
         </b-collapse>
     </b-navbar>
