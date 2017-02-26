e3ec233fa02e09fd3aa5798618cd802954b8521f
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2017-02-26 00:26:30 +0000

    moved roster to playersTable component

--- src/components/models/teams/TeamRoster.vue
+++ src/components/models/teams/TeamRoster.vue
@@ -5,34 +5,24 @@
             <h1>{{team.name}} - Roster</h1>
             <h2>{{team.nationality}}</h2>
         </div>
-        <b-form-fieldset horizontal label="Filter" class="col-6" :label-size="2">
-            <b-form-input v-model="filter" placeholder="Type to Search"></b-form-input>
-        </b-form-fieldset>
-        <b-table stripped hover :items="team.roster" :fields="fields" :filter="filter">
-        </b-table>
+        <players-table :players="team.roster" />
     </div>
 </template>
 <script>
     import TeamsService from '../../../services/ads/TeamsService'
+    import PlayersTable from '../../shared/players/PlayersTable'
+
 
     const service = new TeamsService();
     export default {
         name: 'team-roster',
+        components: {
+            PlayersTable
+        },
         data() {
             return {
                 team: {},
-                fields: {
-                    placeholder: { label: '#', sortable: false },
-                    surname: { label: 'Surname', sortable: true },
-                    name: { label: 'Name', sortable: false },
-                    nationality: { label: 'Nationality', sortable: false },
-                    age: { label: 'Age', sortable: true },
-                    role: { label: 'Role', sortable: false },
-                    skillAvg: { label: 'Skills', sortable: true },
-                    val: { label: 'Value', sortable: false },
-                    actions: { label: 'Actions' }
-                },
-                filter: null
+                players: []
             };
         },
         mounted() {
---
+++ src/components/shared/players/PlayersTable.vue
@@ -0,0 +1,54 @@
+<style></style>
+<template>
+    <div>
+        <table v-if="players.length" class="table table-striped table-hover">
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Name</th>
+                    <th>Nationality</th>
+                    <th>Age</th>
+                    <th>Role</th>
+                    <th>Skills</th>
+                    <th>Value</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="player in players">
+                    <td></td>
+                    <td>{{player.surname}} {{player.name}}</td>
+                    <td>{{player.nationality}}</td>
+                    <td>{{player.age}}</td>
+                    <td>{{player.role}}</td>
+                    <td>{{player.skillAvg}}</td>
+                    <td>{{player.val}}m €</td>
+                    <td>
+                        <b-btn size="sm" variant="secondary" @click="playerDetails(player.id)">
+                            <i class="material-icons">people</i>
+                        </b-btn>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+        <div class="card" v-else>
+            <div class="card-block">
+                No Players ...
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+    export default {
+        name: "singleTeam",
+        props: {
+            players: { type: Array, default: () => [] }
+        },
+        methods: {
+            playerDetails(playerId) {
+                this.$router.push({ name: 'player', params: { playerId } });
+            }
+        }
+    }
+
+</script>
