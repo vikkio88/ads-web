9e64b3f020177388629ea9ecbc5e0142e9380cb1
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2017-02-26 21:57:11 +0000

    adding Round Component

---
+++ src/components/models/leagues/Round.vue
@@ -0,0 +1,38 @@
+<style></style>
+<template>
+    <div>
+        <div class="jumbotron">
+            <h1>
+                <router-link :to="{ name: 'league', params: { leagueId: round.league.id } }">
+                    {{round.league.name}}
+                </router-link>
+            </h1>
+        </div>
+
+        <pre>{{round.matches}}</pre>
+    </div>
+</template>
+<script>
+    import LeaguesService from '../../../services/ads/LeaguesService'
+
+    const service = new LeaguesService();
+    export default {
+        name: 'round',
+        data() {
+            return {
+                round: {}
+            };
+        },
+        mounted() {
+            service.getOneRound(this.$route.params.leagueId, this.$route.params.roundId).then(
+                (data) => {
+                    this.round = data.body.payload;
+                },
+                (error) => {
+                    console.log(error)
+                }
+            );
+        }
+    }
+
+</script>
