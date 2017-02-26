423e19dbe7ea4518757f4b623cf142d3a24c838c
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2017-02-26 01:00:45 +0000

    adding small bottom panel

--- src/App.vue
+++ src/App.vue
@@ -4,15 +4,18 @@
   <div id="app">
     <main-nav />
     <router-view></router-view>
+    <bottom />
   </div>
 </template>
 <script>
   import MainNav from 'components/main/navbars/mainNav';
+  import Bottom from 'components/main/Bottom';
 
   export default {
     name: 'app',
     components: {
-      MainNav
+      MainNav,
+      Bottom
     }
   }
 
---
+++ src/components/main/Bottom.vue
@@ -0,0 +1,18 @@
+<style scoped>
+    small {
+        color: #fff;
+    }
+</style>
+<template>
+    <nav class="navbar fixed-bottom navbar-inverse bg-inverse">
+        <div class="row justify-content-end">
+            <small>made with ♥ by <a href="http://www.vikkio.it" target="_blank">vikkio</a><small>
+        </div>            
+    </nav>
+</template>
+<script>
+    export default {
+        name: 'bottom'
+    }
+
+</script>
