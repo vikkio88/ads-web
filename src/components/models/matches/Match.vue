1f7bb5bd39533472374e092211f04c43601ef4a9
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2017-02-27 20:37:46 +0000

    finalizing Match Component with scorers

--- src/components/models/matches/Match.vue
+++ src/components/models/matches/Match.vue
@@ -2,32 +2,71 @@
 <template>
     <div>
         <div class="jumbotron">
-            <h4>2017-06-24</h4>
-            <h1>Team A 0 - 0 Team B</h1>
-            <h2>League - Round 1</h2>
+            <h4>
+                <router-link :to="{ name: 'league', params: { leagueId: league.id } }">
+                    {{league.name}}
+                </router-link>
+                -
+                <router-link :to="{ name: 'round', params: { leagueId: league.id, roundId: round.id } }">
+                    Round {{round.day}}
+                </router-link>
+            </h4>
+            <h5><small>{{match.date}}</small></h5>
+            <h1 class="display-5">
+                <small><flag :iso="homeTeam.nationality" /></small>
+                <router-link :to="{ name: 'team', params: { teamId: homeTeam.id } }">
+                    {{homeTeam.name}}
+                </router-link>
+                {{result}}
+                <router-link :to="{ name: 'team', params: { teamId: awayTeam.id } }">
+                    {{awayTeam.name}}
+                </router-link>
+                <small><flag :iso="awayTeam.nationality" /></small>
+            </h1>
         </div>
-        <pre>
-            {{scorers}}
-        </pre>
+        <match-scorers :home="homeTeam" :away="awayTeam" :scorers="scorers" />
     </div>
 </template>
 <script>
     import MatchesServices from '../../../services/ads/MatchesServices'
+    import MatchScorers from '../../shared/matches/MatchScorers'
 
     const service = new MatchesServices();
     export default {
         name: 'league',
+        components: {
+            MatchScorers
+        },
         data() {
             return {
+                match: {},
+                homeTeam: {},
+                awayTeam: {},
                 round: {},
                 league: {},
                 scorers: [],
             };
         },
+        computed: {
+            result() {
+                let goalHome = '?';
+                let goalAway = '?';
+                if (this.match.simulated) {
+                    goalHome = this.match.goal_home;
+                    goalAway = this.match.goal_away;
+                }
+                return `${goalHome} - ${goalAway}`;
+            }
+        },
         mounted() {
             service.getOne(this.$route.params.matchId).then(
                 (data) => {
-                    this.league = data.body.payload;
+                    this.match = data.body.payload;
+                    this.homeTeam = this.match.home_team;
+                    this.awayTeam = this.match.away_team;
+                    this.round = this.match.round;
+                    this.league = this.round.league;
+                    this.scorers = this.match.scorers;
                 },
                 (error) => {
                     console.log(error)
