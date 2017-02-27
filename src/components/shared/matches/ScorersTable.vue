5ad6f0278673f040e23a87b0b59074a210ebdd4d
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2017-02-27 20:37:21 +0000

    adding scorer table component

---
+++ src/components/shared/matches/ScorersTable.vue
@@ -0,0 +1,24 @@
+<style></style>
+<template>
+    <table class="table table-striped table-hover">
+        <tbody>
+            <tr v-for="scorer in scorers">
+                <td></td>
+                <td>
+                    <router-link :to="{ name: 'player', params: { playerId: scorer.id } }">
+                        {{scorer.surname}} {{scorer.name}}
+                    </router-link>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</template>
+<script>
+    export default {
+        name: "scorersTable",
+        props: {
+            scorers: { type: Array, default: () => [] }
+        }
+    }
+
+</script>
