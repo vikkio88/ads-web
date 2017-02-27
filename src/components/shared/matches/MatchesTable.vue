f578b580cf823b9130cb00a091d831292a94c650
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2017-02-27 19:52:05 +0000

    adding link to match single view

--- src/components/shared/matches/MatchesTable.vue
+++ src/components/shared/matches/MatchesTable.vue
@@ -26,8 +26,16 @@
                         </router-link>
                         <flag :iso="match.away_team.nationality" />
                     </td>
-                    <td v-if="match.simulated"> {{match.goal_home}} - {{match.goal_away}}</td>
-                    <td v-else=""> ? - ?</td>
+                    <td>
+                        <router-link :to="{ name: 'match', params: { matchId: match.id } }">
+                            <span v-if="match.simulated">
+                                {{match.goal_home}} - {{match.goal_away}}
+                            </span>
+                            <span v-else>
+                                ? - ?
+                            </span>
+                        </router-link>
+                    </td>
                 </tr>
             </tbody>
         </table>
