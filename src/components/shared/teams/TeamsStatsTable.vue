b96919ffb8d46145223b9d7cbd475d4958e2233b
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2017-03-13 17:53:47 +0000

    adding component to show best teams stats

---
+++ src/components/shared/teams/TeamsStatsTable.vue
@@ -0,0 +1,40 @@
+<style>
+</style>
+<template>
+    <div>
+        <table v-if="teams.length" class="table table-striped table-hover">
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Team</th>
+                    <th>Nationality</th>
+                    <th>Won</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="team in teams">
+                    <td></td>
+                    <td>
+                        <router-link :to="{ name: 'team', params: { teamId: team.id } }">
+                            {{team.name}}
+                        </router-link>
+                    </td>
+                    <td>
+                        <flag :iso="team.nationality" />
+                    </td>
+                    <td>
+                        {{team.won}}
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+<script>
+export default {
+    name: "teamsStatsTable",
+    props: {
+        teams: {type: Array, default: () => []},
+    }
+}
+</script>
