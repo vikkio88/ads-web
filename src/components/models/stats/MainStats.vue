ce50b5a42b5da885a0b3c714ba4a9a99b8c42cf2
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2017-03-13 17:53:10 +0000

    adding teams stats

--- src/components/models/stats/MainStats.vue
+++ src/components/models/stats/MainStats.vue
@@ -1,21 +1,42 @@
 <style></style>
 <template>
 <div>
-    <players-stats-table :players="stats.players" />
-<pre>
-{{stats}}
-</pre>
+    <div class="jumbotron">
+            <h1>Statistics</h1>
+    </div>
+    <div class="card">
+        <h3 class="card-header">Scorers Table</h3>
+        <div class="card-block">
+            <players-stats-table :players="stats.scorers" stat="goals"/>
+        </div>
+    </div>
+
+    <div class="card">
+        <h3 class="card-header">Best Performances</h3>
+        <div class="card-block">
+            <players-stats-table :players="stats.avg" stat="avg"/>
+        </div>
+    </div>
+
+    <div class="card">
+        <h3 class="card-header">Best Teams</h3>
+        <div class="card-block">
+            <teams-stats-table :teams="stats.teams" />
+        </div>
+    </div>
 </div>
 </template>
 <script>
     import StatsService from '../../../services/ads/StatsService'
     import PlayersStatsTable from '../../shared/players/PlayersStatsTable'
+    import TeamsStatsTable from '../../shared/teams/TeamsStatsTable'
 
     const service = new StatsService();
     export default {
         name: "mainStats",
         components:{
-            PlayersStatsTable
+            PlayersStatsTable,
+            TeamsStatsTable
         },
         data(){
             return {
