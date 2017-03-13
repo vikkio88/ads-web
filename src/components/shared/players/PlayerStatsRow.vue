8856db15d747579b9c6c2f08c0adf3cadfb01b4c
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2017-03-13 08:17:21 +0000

    addded component for players stats

---
+++ src/components/shared/players/PlayerStatsRow.vue
@@ -0,0 +1,28 @@
+<style>
+</style>
+<template>
+<tr>
+    <td></td>
+    <td>
+        <router-link :to="{ name: 'player', params: { playerId: player.id } }">
+            {{player.surname}} {{player.name}}
+        </router-link>
+    </td>
+    <td>
+        <flag :iso="player.nationality" />
+    </td>
+    <td>{{team.name}}</td>
+    <td>{{player.appearances}}</td>
+    <td>{{player.goals}}</td>
+    <td>{{player.avg}}</td>
+</tr>
+</template>
+<script>
+export default {
+    name: "playerStatsRow",
+    props: {
+        player: {type: Object, default: () => {}},
+        team: {type: Object, default: () => {}}
+    }
+}
+</script>
---
+++ src/components/shared/players/PlayersStatsTable.vue
@@ -0,0 +1,40 @@
+<style>
+</style>
+<template>
+<div>
+        <table v-if="players.length" class="table table-striped table-hover">
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Name</th>
+                    <th>Nationality</th>
+                    <th>Team</th>
+                    <th>Appearances</th>
+                    <th>Goals</th>
+                    <th>Avg</th>
+                </tr>
+            </thead>
+            <tbody>
+                <player-stats-row :player="player" :team="player.team" v-for="player in players"/>
+            </tbody>
+        </table>
+        <div class="card" v-else>
+            <div class="card-block">
+                No Players ...
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+import PlayerStatsRow from './PlayerStatsRow'
+
+export default {
+    name: "playersStatsTable",
+    components:{
+        PlayerStatsRow
+    },
+    props: {
+        players: {type: Array, default: () => []}
+    }
+}
+</script>
